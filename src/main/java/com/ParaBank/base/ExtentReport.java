a5d632a692df3fa573632c92ec25b40c140977ba
govindmaddala <99980734+govindmaddala@users.noreply.github.com>
2022-05-24 15:36:55 +0530

    Add files via upload

---
+++ src/main/java/com/ParaBank/base/ExtentReport.java
@@ -0,0 +1,23 @@
+package com.ParaBank.base;
+
+import org.testng.annotations.BeforeTest;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentReport 
+{
+	static ExtentReports er=new ExtentReports();
+	
+	@BeforeTest
+	public static ExtentReports Reports()
+	{
+		String report_path=System.getProperty("user.dir")+"\\reports\\index.html";
+		ExtentSparkReporter esr=new ExtentSparkReporter(report_path);
+		esr.config().setReportName("ParaBank Automation report Details");
+		esr.config().setDocumentTitle("ParaBank Project Report");
+		er.attachReporter(esr);
+		er.setSystemInfo("Tester", "Govind Maddala");
+		return er;
+	}
+}
